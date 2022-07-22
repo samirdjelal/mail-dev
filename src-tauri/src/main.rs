189f6fcbdd0624f15d1fa5b8cfba7960ea1d36a1
wei.cai <wei.cai@mdpi.com>
2022-07-22 14:05:53 +0800

    update crate

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,26 +1,26 @@
 #![cfg_attr(
-all(not(debug_assertions), target_os = "windows"),
-windows_subsystem = "windows"
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
 )]
 
+mod forward;
 mod smtp;
 mod window;
-mod forward;
 
-use smtp::start_smtp_server;
 use forward::forward_mail;
+use smtp::start_smtp_server;
 use tauri::Manager;
 
 fn main() {
-	tauri::Builder::default()
-		.setup(|app| {
-			let _s = window::main_window(Some(app.get_window("main").unwrap()));
-			// app.manage(MainWindow(Arc::new(Mutex::new(
-			// 	app.get_window("main").unwrap(),
-			// ))));
-			Ok(())
-		})
-		.invoke_handler(tauri::generate_handler![start_smtp_server, forward_mail])
-		.run(tauri::generate_context!())
-		.expect("error while running tauri application");
+  tauri::Builder::default()
+    .setup(|app| {
+      let _s = window::main_window(Some(app.get_window("main").unwrap()));
+      // app.manage(MainWindow(Arc::new(Mutex::new(
+      // 	app.get_window("main").unwrap(),
+      // ))));
+      Ok(())
+    })
+    .invoke_handler(tauri::generate_handler![start_smtp_server, forward_mail])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
--- src-tauri/src/window.rs
+++ src-tauri/src/window.rs
@@ -1,14 +1,16 @@
-use tauri::Window;
 use std::sync::Once;
+use tauri::Window;
 
 pub(crate) fn main_window(window: Option<Window>) -> Window {
-	static mut SINGLETON: *const Window = 0 as *const Window;
-	static ONCE: Once = Once::new();
-	if window.is_some() {
-		ONCE.call_once(|| {
-			let main_window = window.unwrap();
-			unsafe { SINGLETON = std::mem::transmute(Box::new(main_window)); }
-		});
-	}
-	unsafe { (*SINGLETON).clone() }
+  static mut SINGLETON: *const Window = 0 as *const Window;
+  static ONCE: Once = Once::new();
+  if window.is_some() {
+    ONCE.call_once(|| {
+      let main_window = window.unwrap();
+      unsafe {
+        SINGLETON = std::mem::transmute(Box::new(main_window));
+      }
+    });
+  }
+  unsafe { (*SINGLETON).clone() }
 }
